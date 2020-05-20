eb4d83abf91a78ab92662e7cf01c9a9b5ca220a0
Reznychenko Sergey <sergeyrez.sr@gmail.com>
2020-05-20 13:05:35 +0300

    HW_3

---
+++ MVCSample/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCSample.Models;
+using System.Linq;
+
+namespace MVCSample.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (NewsBase.News.Any())
+                ViewData["NewsList"] = NewsBase.News;
+            else
+                return new NotFoundResult();
+            return View();
+        }
+    }
+}
